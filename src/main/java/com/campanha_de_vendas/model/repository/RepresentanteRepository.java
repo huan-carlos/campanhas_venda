c98f4c5d59509638ddd6c063bdd50f4f7548076d
Huan Carlos <huanc65@gmail.com>
2023-06-26 22:44:25 -0300

    JPARepository + Busca por Nome

--- src/main/java/com/campanha_de_vendas/model/repository/RepresentanteRepository.java
+++ src/main/java/com/campanha_de_vendas/model/repository/RepresentanteRepository.java
@@ -3,5 +3,8 @@ package com.campanha_de_vendas.model.repository;
 import com.campanha_de_vendas.model.entity.Representante;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RepresentanteRepository extends JpaRepository<Representante, Long> {
+    List<Representante> findAllByNomeContainingIgnoreCase(String nome);
 }

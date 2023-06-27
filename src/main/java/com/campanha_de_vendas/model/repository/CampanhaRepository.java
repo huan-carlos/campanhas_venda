d41b6fa8e67e26a7b3251384c0b9a17f509f5f68
Huan Carlos <huanc65@gmail.com>
2023-06-26 22:43:50 -0300

    JPARepository + Metodo Busca Data Atual

--- src/main/java/com/campanha_de_vendas/model/repository/CampanhaRepository.java
+++ src/main/java/com/campanha_de_vendas/model/repository/CampanhaRepository.java
@@ -3,5 +3,9 @@ package com.campanha_de_vendas.model.repository;
 import com.campanha_de_vendas.model.entity.Campanha;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface CampanhaRepository extends JpaRepository<Campanha, Long> {
+    List<Campanha> findAllByDataFimAfter(LocalDate dataFim);
 }
